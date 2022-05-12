64823d3ad8b8f2087b2426a3e7ac1392058fc515
esethubob <105235285+esethubob@users.noreply.github.com>
2022-05-12 15:56:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
